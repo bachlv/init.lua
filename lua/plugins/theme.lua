a0e9685b009de4d560e8e8cb7acc32a924b1ba59
Bách Lê <bach.le@codelink.io>
2024-12-28 16:58:53 +0700

    Update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,21 +6,10 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "auto", -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
+        -- extend_background_behind_borders = true,
 
         enable = {
           terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = false,
         },
 
         groups = {
@@ -78,6 +67,14 @@ return {
 
           DashboardHeader = { fg = "highlight_high" },
           DashboardFooter = { fg = "highlight_high" },
+          NotifyERRORBorder = { bg = "base" },
+          NotifyINFOBorder = { bg = "base" },
+          NotifyTRACEBorder = { bg = "base" },
+          NotifyWARNBorder = { bg = "base" },
+
+          ["@property"] = { italic = false },
+          ["@variable"] = { italic = false },
+
           -- Comment = { fg = "foam" },
           -- VertSplit = { fg = "muted", bg = "muted" },
         },
@@ -99,10 +96,7 @@ return {
         --   BufferLineIndicatorSelected = { fg = "iris", bg = "base" },
         --   BufferLineOffsetSeparator = { fg = "muted", bg = "base" },
         --
-        --   NotifyERRORBorder = { bg = "base" },
-        --   NotifyINFOBorder = { bg = "base" },
-        --   NotifyTRACEBorder = { bg = "base" },
-        --   NotifyWARNBorder = { bg = "base" },
+
         --
         --   -- Pmenu = { bg = "highlight_" },
         --   -- CmpNormal = { bg = "base" },
